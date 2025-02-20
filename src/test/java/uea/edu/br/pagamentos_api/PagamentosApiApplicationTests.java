e04680b8c31de5fab1c6ba90c7e248973e3bb68d
Henrique <henrixe80@gmail.com>
2025-02-19 20:32:24 -0400

    Configuração Inicial

---
+++ src/test/java/uea/edu/br/pagamentos_api/PagamentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uea.edu.br.pagamentos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
