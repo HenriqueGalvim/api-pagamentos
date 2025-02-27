d75a1fc1fccc9437b629ffa350a28f62b0870be2
Henrique <henrixe80@gmail.com>
2025-02-26 21:03:08 -0400

    2. Criação das entidades e mapeamento

--- src/main/java/uea/edu/br/pagamentos_api/model/Categoria.java
+++ src/main/java/uea/edu/br/pagamentos_api/model/Categoria.java
@@ -1,13 +1,27 @@
 package uea.edu.br.pagamentos_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
-    private String nome; //Testando
+
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String nome;
 }
